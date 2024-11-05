6ebf487ac94d2d6e3ea8ac5ab35dc4a679eee147
mamancha <mark.mancha13@gmail.com>
2024-11-05 17:54:11 -0600

    Login
    Username, Password, Login button, create Account button, Title, MainMenuManger

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class MainMenuManager: MonoBehaviour
+{
+    [SerializeField] private Canvas mainMenu;
+    [SerializeField] private Canvas createAccount;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateAccount()
+    {
+        mainMenu.enabled = false;
+        createAccount.enabled = true;
+    }
+
+    public void MainMenu()
+    {
+        mainMenu.enabled = true;
+        createAccount.enabled = false;
+    }
+
+    public void Login()
+    {
+        //Code to verify login credentials
+        //if doesnt match or doesnt exist error
+    }
+
+    public void Options()
+    {
+        //Code to show settings canvas
+    }
+    
+    
+    
+}

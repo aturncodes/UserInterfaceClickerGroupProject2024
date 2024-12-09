34e7a237612f7c3e4c30966e862037e902ac1112
mamancha <mark.mancha13@gmail.com>
2024-12-08 18:18:06 -0600

    settings and achievements fix

--- Assets/Scripts/MainMenu/sceneChanger.cs
+++ Assets/Scripts/MainMenu/sceneChanger.cs
@@ -1,35 +1,40 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class sceneChanger : MonoBehaviour
 {
 
+    [SerializeField] private CanvasGroup settingsMenu;
+    [SerializeField] private CanvasGroup achievements;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        generators();
     }
-    public void upgradeChange(){
-        Debug.Log("Upgrade Button Clicked");
-        SceneManager.LoadScene("Upgrades");
+    public void settings(){
+        Debug.Log($"Before Change - Alpha: {settingsMenu.alpha}, Interactable: {settingsMenu.interactable}, BlocksRaycasts: {settingsMenu.blocksRaycasts}");
+
+        settingsMenu.alpha = 1f;           
+        settingsMenu.interactable = true;  
+        settingsMenu.blocksRaycasts = true; 
+
+        Debug.Log($"After Change - Alpha: {settingsMenu.alpha}, Interactable: {settingsMenu.interactable}, BlocksRaycasts: {settingsMenu.blocksRaycasts}");
     }
-    public void managerChange(){
-        SceneManager.LoadScene("Managers");
+    public void acheivements(){
+        achievements.alpha = 1f;           
+        achievements.interactable = true;  
+        achievements.blocksRaycasts = true; 
     }
-    public void settingsChange(){
-        SceneManager.LoadScene("Settings");
-    }
-    public void acheivementsChange(){
-        SceneManager.LoadScene("Achievements");
-    }
-    public void generatorsChange(){
-        SceneManager.LoadScene("Game");
+    public void generators(){
+        settingsMenu.alpha = 0f;           
+        settingsMenu.interactable = false;  
+        settingsMenu.blocksRaycasts = false;
+        achievements.alpha = 0;           
+        achievements.interactable = false;  
+        achievements.blocksRaycasts = false; 
     }
 
     public void quitGame() {
